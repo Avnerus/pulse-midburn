669e76fa4ead82df7c04eeaf1ec778cafd29d55a
Amit Friedberg <amitos80@gmail.com>
2014-04-18 18:40:34 +0300

    - added Physijs

--- visual/World.js
+++ visual/World.js
@@ -13,34 +13,67 @@ function World(args){
 // Class constructor
 World.prototype.init = function (args) {
     // Set the different geometries composing the room
-    var ground = new THREE.PlaneGeometry(1024, 2048),
-        height = 128,
-        walls = [
-//            new THREE.PlaneGeometry(ground.height, height),
-//            new THREE.PlaneGeometry(ground.width, height),
-//            new THREE.PlaneGeometry(ground.height, height),
-//            new THREE.PlaneGeometry(ground.width, height)
-        ],
-        obstacles = [
-            new THREE.CubeGeometry(64, 64, 64)
-        ],
-    // Set the material, the "skin"
-        material = new THREE.MeshLambertMaterial(args),
-        i;
+//    var ground = new THREE.PlaneGeometry(1024, 2048),
+//        height = 128,
+//        walls = [
+////            new THREE.PlaneGeometry(ground.height, height),
+////            new THREE.PlaneGeometry(ground.width, height),
+////            new THREE.PlaneGeometry(ground.height, height),
+////            new THREE.PlaneGeometry(ground.width, height)
+//        ],
+//        obstacles = [
+//            new THREE.CubeGeometry(64, 64, 64)
+//        ],
+//    // Set the material, the "skin"
+//        material = new THREE.MeshLambertMaterial(args),
+//        i;
+//
+//    // Set the "world" modelisation object
+//    this.mesh = new THREE.Object3D();
+//
+//    // Set and add the ground
+//    this.ground = new THREE.Mesh(ground, material);
+//    this.ground.rotation.x = -Math.PI / 2;
+//
+//    this.mesh.add(this.ground);
 
-    // Set the "world" modelisation object
+////////////////////////////////////////////////////////////////////////
+
+    var material = Physijs.createMaterial(
+        new THREE.MeshLambertMaterial(args),
+        .0, // high friction
+        .0 // low restitution
+    );
+
+//    material.map.wrapS = material.map.wrapT = THREE.RepeatWrapping;
+//    material.map.repeat.set( 3, 3 );
+
+    this.ground = new Physijs.BoxMesh(
+        new THREE.CubeGeometry(500, 200, 500),
+        material,
+        0 // mass
+    );
+
     this.mesh = new THREE.Object3D();
-    // Set and add the ground
-    this.ground = new THREE.Mesh(ground, material);
-    this.ground.rotation.x = -Math.PI / 2;
+
+//    this.ground.rotation.x = -Math.PI / 2;
+
     this.mesh.add(this.ground);
+
+
+
+////////////////////////////////////////////////////////////////////////
+
+
     // Set and add the walls
-    this.walls = [];
-    for (i = 0; i < walls.length; i += 1) {
-        this.walls[i] = new THREE.Mesh(walls[i], material);
-        this.walls[i].position.y = height / 2;
-        this.mesh.add(this.walls[i]);
-    }
+//    this.walls = [];
+//    for (i = 0; i < walls.length; i += 1) {
+//        this.walls[i] = new THREE.Mesh(walls[i], material);
+//        this.walls[i].position.y = height / 2;
+//        this.mesh.add(this.walls[i]);
+//    }
+
+
 //    this.walls[0].rotation.y = -Math.PI / 2;
 //    this.walls[0].position.x = ground.width / 2;
 
